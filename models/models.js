78ed10fac62b70c0f86551d71ec8c0a6131b0d06
Antonio Teixeira <antonio.tex@gmail.com>
2015-06-09 10:38:37 +0200

    Cambio de SqLite a MySql por problemas con debian

--- models/models.js
+++ models/models.js
@@ -1,10 +1,28 @@
 var path = require('path');
 
+//var DATABASE_URL = 'mysql://root:adclick@192.168.50.223:3306/quiz';
+//var url = DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user    = (url[2]||null);
+var pwd     = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+
 //Carga modelo ORM
 var Sequelize = require('sequelize');
 
 //Usar SQLite
-var sequelize = new Sequelize(null, null, null, {dialect:"node_sqlite", storage:"quiz.sqlite"});
+var sequelize = new Sequelize(DB_name, user, pwd, 
+{ dialect: protocol,
+  protocol: protocol,
+  port: port,
+  host: host,
+  omitNull: true
+});
 
 //Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
