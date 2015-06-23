f800238dec7308e46b7580548613b4b8d5ccd4eb
Antonio Teixeira <antonio.tex@gmail.com>
2015-06-23 16:23:14 +0200

    Puesta caducidad de la cookie a 2 minutos

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser('Quiz 2015'));
+app.use(express.session({cookie: {maxAge: 120000}}));//tiempo de expiración de la sesión
 app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
